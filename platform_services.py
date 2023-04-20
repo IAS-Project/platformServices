c034cf5779e96e075ed25e777838f9eee904cb96
lokeshsharma123456 <lokeshsharma123456@gmail.com>
2023-04-20 21:27:18 +0530

    update of try exceptin

--- platform_services.py
+++ platform_services.py
@@ -1,5 +1,17 @@
-from twilio.rest import Client
-
+#for whatsapp services 
+# !pip install twilio
+# from twilio.rest import Client
+# !pip install smtplib
+import smtplib
+#for email services
+#email of sender and password 
+#its a random generated password from google account
+#it has to be done manually
+sender_email = "sharmameritnation@gmail.com"
+sender_psw = 'pkjvtsmvofuqpjaz'
+#------------till now manual otherwise a function for
+#------------taking arguments is required
+'''
 def send_whatsapp_notification(to_number, message, account_sid, auth_token):
     client = Client(account_sid, auth_token)
 
@@ -18,13 +30,14 @@ account_sid = "ACef0a0280e62b5f9afeb8707d73b3053d"
 auth_token = "66ec1a3be77bfc9446a246839b4f7ac0"
 
 send_whatsapp_notification(to_number, message, account_sid, auth_token)
+'''
+#------------------send message completed---------------------
+#------------------send emails started ------------------------
 
-
-psw = 'pkjvtsmvofuqpjaz'
 emails = [
     {
         "email": "gandhisanya99@gmail.com",
-        "Sub": "Fail system",
+        "Sub": "sorry guys ignore maro",
         "Body": {
             "name": "Load Balancer",
             "Containerid": "122",
@@ -34,7 +47,7 @@ emails = [
     },
     {
         "email": "yashsampat23154@gmail.com",
-        "Sub": "Warning",
+        "Sub": "sorry guys ignore maro",
         "Body": {
             "name": "Web Server",
             "Containerid": "234",
@@ -44,31 +57,51 @@ emails = [
     }
 ]
 
-import smtplib
+
 def send_mail(sender_email , receiver_email,password,sub, body):
     message = f"Subject: {sub} \n\n{body}"
-    # message += f"Name of subsystem is {body['name']}\n"
+    '''# message += f"Name of subsystem is {body['name']}\n"
     # message += f"Container id  is {body['Containerid']}\n"
     # message += f"VMhost  is {body['VMhost']}\n\n"
     # message += 'This is a testing message now it worked,\n tell the changes and the format' 
-    #create connection
-    server = smtplib.SMTP('smtp.gmail.com')
-    #start TLS
-    server.starttls()
-    print('Connectin made successfully')
+    #create connection'''
+    try:
+        server = smtplib.SMTP('smtp.gmail.com')
+        #start TLS
+        server.starttls()
+        # print('Connectin made successfully')
 
-    server.login(sender_email, password)
-    print('Login successful to server')
-    # Send the email
-    server.sendmail(sender_email, receiver_email, message)
-    print(f'message send successfully to {receiver_email}')
+        try:
+            server.login(sender_email, password)
+            # print('Login successful to server')
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            print("Failed to login. Please check your credentials and try again.")
+            return
+            
+        try:
+            # Send the email
+            result = server.sendmail(sender_email, receiver_email, message)
+            if not result:
+                print(f'message send successfully to {receiver_email}')
+            else:
+                print(f'Failed to send email to {result.keys()}')
 
-    server.quit()
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            print("Failed to send email. Please check the email addresses provided and try again.")
+            return
+        finally:
+            #close the connection
+            server.quit()
+    except Exception as e:
+        print(f"Error {str(e)}")
+        print("Failed to connect to Smtp. Please check internet connectivity")
     
 for email in emails:
-    sender_email = "sharmameritnation@gmail.com"
+    sender_email = sender_email
+    password = sender_psw
     receiver_email = email["email"]
-    password = psw
     subject = email["Sub"]
     body = f"Name: {email['Body']['name']}\nContainer ID: {email['Body']['Containerid']}\nVM Host: {email['Body']['VMhost']}\nStatus: {email['status']}"
     send_mail(sender_email, receiver_email, password, subject, body)
