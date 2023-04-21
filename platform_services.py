f4fb753e366c674e488c9eb7a1e959da4e37ecf6
SanyaGandhi <gandhisanya99@gmail.com>
2023-04-21 16:40:18 +0530

    documentation, commenting, instructions added'

--- platform_services.py
+++ platform_services.py
@@ -1,19 +1,12 @@
 
+#-------------------- Libraries for Email----------------------------------------------------- 
 import smtplib
-#for email services
-#email of sender and password 
-#its a random generated password from google account
-#it has to be done manually
-sender_email = "sharmameritnation@gmail.com" #this is constant
-sender_psw = 'pkjvtsmvofuqpjaz' #this is constant
-#------------till now manual otherwise a function for
-#------------taking arguments is required
-
+#--------------------Start code for WhatsApp----------------------------------------------------- 
 from twilio.rest import Client
 
-def send_whatsapp_notification(to_number, message, account_sid, auth_token):
+def send_whatsapp_notification(sender, to_number, message, account_sid, auth_token):
     client = Client(account_sid, auth_token)
-
+    message=f'Hey there! \nYou have been contacted through IAS group 5.\nYou have received a message from {sender}. \nThe message is{message}'
     message = client.messages.create( 
         body=message,
         from_='whatsapp:+14155238886',
@@ -23,18 +16,46 @@ def send_whatsapp_notification(to_number, message, account_sid, auth_token):
     print(f"WhatsApp notification sent to {to_number}, Message SID: {message.sid}")
 
 # If you wish to change the 'to_number', make sure you send 'join appearance-stove' to '+14155238886'
-to_number = '+919887903310'
-message = "Hello, this is a notification message!"
+to_number = '+917506636566'
+message = "The message is from Fault Tolerance module. There is a problem with node sid_vm!"
+senderName='Sanya Gandhi'
+
 # Account has been already created. So, use the following
 account_sid = "ACef0a0280e62b5f9afeb8707d73b3053d"
 auth_token = "66ec1a3be77bfc9446a246839b4f7ac0"
 
 
 # For testing purposes
-send_whatsapp_notification(to_number, message, account_sid, auth_token)
+send_whatsapp_notification(senderName ,to_number, message, account_sid, auth_token)
 
-#------------------send message completed---------------------
-#------------------send emails started ------------------------
+#------------------------End code for WhatsApp-----------------
+
+
+#------------------Start code for Email ------------------------
+# In order to access mail services, call 'send_mail' function
+# Parameters:
+# 1. Sender email address
+# 2. Receiver email address
+# 3. Your app password
+## Generate app password by following the steps below:
+#       Go to your google account
+#       Go to security
+#       Go to 2- step verification
+#       Create an 'App password' and use the password here
+#       Add Subject string
+#       Add message body
+
+def send_mail(sender_email , receiver_email, password, sub = "Welcome buddies", body = "Thanks for using our platform, please check the code,\n updated in github"):
+    #Broadcasting message to all if it is a list
+    if isinstance(receiver_email, list):
+        for receiver in receiver_email:
+            # Send email to each receiver
+            send(sender_email , receiver, password, sub, body)
+    else:
+        # Send email to single receiver
+        send(sender_email , receiver_email, password, sub, body)
+         
+
 
 def send(sender_email ,receiver_email, password, sub = "", body = ""):
     #sender_email : email of sender
@@ -72,20 +93,8 @@ def send(sender_email ,receiver_email, password, sub = "", body = ""):
     except Exception as e:
         print(f"Error {str(e)}")
         print("Failed to connect to Smtp. Please check internet connectivity")
+
+
         
-def send_mail(sender_email , receiver_email, password, sub = "Welcome buddies", body = "Thanks for using our platform, please check the code,\n updated in github"):
-    #Broadcasting message to all if it is a list
-    if isinstance(receiver_email, list):
-        for receiver in receiver_email:
-            # Send email to each receiver
-            send(sender_email , receiver, password, sub, body)
-    else:
-        # Send email to single receiver
-        send(sender_email , receiver_email, password, sub, body)
-         
-    
-# x = ["gandhisanya99@gmail.com", "lokeshsharma123456@gmail.com","yashsampat23154@gmail.com"]
-# y = "lokeshsharma123456@gmail.com"
-# send_mail(sender_email, x, sender_psw)
 
  
